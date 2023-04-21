0ea9fcb09679c88089b132747985659d0b3ad78e
vincentsgherzi <sgherzivincent@gmail.com>
2023-04-21 10:17:08 -0700

    add display method

--- LAB3/src/App.java
+++ LAB3/src/App.java
@@ -1,7 +1,27 @@
 package LAB3.src;
 
 public class App {
-    public static void main(String[] args) throws Exception{
-        System.out.println("Hello, World!");
+
+
+
+    public static void main(String[] args) throws Exception {
+        display ("This is my display!");
+    }
+
+    public static void display(String message) {
+
+        int length = message.length();
+
+        String border = "+";
+
+        for (int i = 0; i < length + 2; i++) {
+            border += "_";
+        }
+
+        border += "+";
+        
+        System.out .println(border);
+        System.out.println("| " + message + " |");
+        System.out.println (border);
     }
 }
